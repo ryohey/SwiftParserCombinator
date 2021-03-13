245e1ddad400a04a9be9f0018d383b675151ef86
ryohey <info@codingcafe.jp>
2021-03-13 13:01:21 +0900

    Parser as function

--- Sources/SwiftParserCombinator/SwiftParserCombinator.swift
+++ Sources/SwiftParserCombinator/SwiftParserCombinator.swift
@@ -7,7 +7,6 @@ import Foundation
 //    func read() -> T
 //}
 //
-//typealias Parser<I, O> = (I) throws -> O
 //
 //func ||<I, O>(a: @escaping Parser<I, O>, b: @escaping Parser<I, O>) -> Parser<I, O> {
 //    return {
@@ -23,6 +22,8 @@ import Foundation
 //
 //}
 
+typealias Parser<I, O> = (Iterated<I>) throws -> Iterated<O>
+
 enum ParseError: Error {
     case notImpelemented
 }
@@ -36,58 +37,20 @@ struct Iterated<T> {
     let position: Int
 }
 
-protocol Parser {
-    associatedtype Input
-    associatedtype Output
-    
-    func parse(_ input: Iterated<Input>) throws -> Iterated<Output>
-}
-
-struct OrParser<P1: Parser, P2: Parser>: Parser where
-    P1.Input == P2.Input,
-    P1.Output == P2.Output {
-    typealias Input = P1.Input
-    typealias Output = P2.Output
-    
-    private let a: P1
-    private let b: P2
-    
-    init(_ a: P1, _ b: P2){
-        self.a = a
-        self.b = b
-    }
-    
-    func parse(_ input: Iterated<Input>) throws -> Iterated<Output> {
+func ||<Input, Output>(a: @escaping Parser<Input, Output>, b: @escaping Parser<Input, Output>) -> Parser<Input, Output> {
+    return { input in
         do {
-            return try a.parse(input)
+            return try a(input)
         } catch {
-            return try b.parse(input)
+            return try b(input)
         }
     }
 }
 
-func ||<P1: Parser, P2: Parser>(a: P1, b: P2) -> OrParser<P1, P2> where
-    P1.Input == P2.Input,
-    P1.Output == P2.Output {
-    return OrParser(a, b)
-}
-
-struct SequenceParser<P1: Parser, P2: Parser>: Parser where
-    P2.Input == P1.Input {
-    typealias Input = P1.Input
-    typealias Output = (P1.Output, P2.Output)
-    
-    private let a: P1
-    private let b: P2
-    
-    init(_ a: P1, _ b: P2){
-        self.a = a
-        self.b = b
-    }
-    
-    func parse(_ input: Iterated<Input>) throws -> Iterated<Output> {
-        let output1 = try a.parse(input)
-        let output2 = try b.parse(Iterated(value: input.value, position: output1.position))
+func +<Input, Output1, Output2>(a: @escaping Parser<Input, Output1>, b: @escaping Parser<Input, Output2>) -> Parser<Input, (Output1, Output2)> {
+    return { input in
+        let output1 = try a(input)
+        let output2 = try b(Iterated(value: input.value, position: output1.position))
         return Iterated(
             value: (output1.value, output2.value),
             position: output2.position
@@ -95,55 +58,9 @@ struct SequenceParser<P1: Parser, P2: Parser>: Parser where
     }
 }
 
-struct TupleParser<P1: Parser, P2: Parser, O1, O2>: Parser where
-    P1.Output == (O1, O2),
-    P2.Input == P1.Input {
-    typealias Input = P1.Input
-    typealias Output = (O1, O2, P2.Output)
-    
-    private let a: P1
-    private let b: P2
-    
-    init(_ a: P1, _ b: P2){
-        self.a = a
-        self.b = b
-    }
-    
-    func parse(_ input: Iterated<Input>) throws -> Iterated<Output> {
-        let output1 = try a.parse(input)
-        let output2 = try b.parse(Iterated(value: input.value, position: output1.position))
-        return Iterated(
-            value: (output1.value.0, output1.value.1, output2.value),
-            position: output2.position
-        )
-    }
-}
-//
-//func +<P1: Parser, P2: Parser, O1, O2>(a: P1, b: P2) -> TupleParser<P1, P2, O1, O2> where
-//    P1.Output == (O1, O2),
-//    P2.Input == P1.Input {
-//    return TupleParser(a, b)
-//}
-//
-//func +<P1: , P2: Parser>(a: SequenceParser, b: P2) -> TupleParser<P1, P2> where P2.Input == P1.Input {
-//    return TupleParser(a, b)
-//}
-
-func +<P1: Parser, P2: Parser>(a: P1, b: P2) -> SequenceParser<P1, P2> where P2.Input == P1.Input {
-    return SequenceParser(a, b)
-}
-
-struct MapParser<P: Parser, Output>: Parser {
-    private let parser: P
-    private let fn: (P.Output) throws -> Output
-    
-    init(_ parser: P, _ fn: @escaping (P.Output) throws -> Output) {
-        self.parser = parser
-        self.fn = fn
-    }
-    
-    func parse(_ input: Iterated<P.Input>) throws -> Iterated<Output> {
-        let result = try parser.parse(input)
+func map<Input, Output1, Output2>(_ parser: @escaping Parser<Input, Output1>, _ fn: @escaping (Output1) throws -> Output2) -> Parser<Input, Output2> {
+    return { input in
+        let result = try parser(input)
         return Iterated(
             value: try fn(result.value),
             position: result.position
@@ -151,47 +68,13 @@ struct MapParser<P: Parser, Output>: Parser {
     }
 }
 
-func map<P: Parser, Output>(_ parser: P, _ fn: @escaping (P.Output) throws -> Output) -> MapParser<P, Output> {
-    return MapParser(parser, fn)
-}
-
 extension StringProtocol {
     subscript(offset: Int) -> Character { self[index(startIndex, offsetBy: offset)] }
 }
 
-struct CharParser: Parser {
-    typealias Input = String
-    typealias Output = String
-    
-    private let char: Character
-    
-    init(_ char: Character) {
-        self.char = char
-    }
-    
-    func parse(_ input: Iterated<Input>) throws -> Iterated<Output> {
-        guard input.position < input.value.count else {
-            throw "\(input.position) is out of string range"
-        }
-        let c = input.value[input.position]
-        guard c == char else {
-            throw "\(c) is not \(char)"
-        }
-        return Iterated(value: "\(char)", position: input.position + 1)
-    }
-}
-
-struct CharRangeParser: Parser {
-    typealias Input = String
-    typealias Output = String
-    
-    private let range: ClosedRange<UInt32>
-    
-    init(_ from: Unicode.Scalar, _ to: Unicode.Scalar) {
-        range = UInt32(from)...UInt32(to)
-    }
-    
-    func parse(_ input: Iterated<Input>) throws -> Iterated<Output> {
+func charRange(_ from: Unicode.Scalar, _ to: Unicode.Scalar) -> Parser<String, String> {
+    let range = UInt32(from)...UInt32(to)
+    return { input in
         guard input.position < input.value.count else {
             throw "\(input.position) is out of string range"
         }
@@ -204,6 +87,15 @@ struct CharRangeParser: Parser {
     }
 }
 
-func char(_ c: Character) -> CharParser {
-    return CharParser(c)
+func char(_ char: Character) -> Parser<String, String> {
+    return { input in
+        guard input.position < input.value.count else {
+            throw "\(input.position) is out of string range"
+        }
+        let c = input.value[input.position]
+        guard c == char else {
+            throw "\(c) is not \(char)"
+        }
+        return Iterated(value: "\(char)", position: input.position + 1)
+    }
 }
--- Tests/SwiftParserCombinatorTests/SwiftParserCombinatorTests.swift
+++ Tests/SwiftParserCombinatorTests/SwiftParserCombinatorTests.swift
@@ -6,8 +6,8 @@ final class SwiftParserCombinatorTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        let hash = CharParser("#")
-        let hex = CharRangeParser("0", "9") || CharRangeParser("a", "f")
+        let hash = char("#")
+        let hex = charRange("0", "9") || charRange("a", "f")
         let component = map(hex + hex, { (tuple: (String, String)) -> Int in
             let str = tuple.0 + tuple.1
             var rgbValue: UInt64 = 0
@@ -18,18 +18,19 @@ final class SwiftParserCombinatorTests: XCTestCase {
         let g = component
         let b = component
         let gb = g + b
+        let hrgb = hash + r + g + b
         let colorParser = map(hash + r + g + b, { (tuple: (((String, Int), Int), Int)) in
             return (r: tuple.0.0.1, g: tuple.0.1, b: tuple.1)
         })
         let input = Iterated(value: "#ff6400", position: 0)
-        let result = try colorParser.parse(input)
+        let result = try colorParser(input)
         XCTAssertEqual(result.value.r, 255)
         XCTAssertEqual(result.value.g, 100)
         XCTAssertEqual(result.value.b, 0)
         XCTAssertEqual(result.position, 7)
-        XCTAssertThrowsError(try colorParser.parse(Iterated(value: "#ff400", position: 0)))
-        XCTAssertThrowsError(try colorParser.parse(Iterated(value: "#ff400_", position: 0)))
-        XCTAssertThrowsError(try colorParser.parse(Iterated(value: "ff4000", position: 0)))
+        XCTAssertThrowsError(try colorParser(Iterated(value: "#ff400", position: 0)))
+        XCTAssertThrowsError(try colorParser(Iterated(value: "#ff400_", position: 0)))
+        XCTAssertThrowsError(try colorParser(Iterated(value: "ff4000", position: 0)))
     }
 
     static var allTests = [
